42e6f6fad656affb0b692736040231186ba43f84
saket mishra <mishrasaket042@gmail.com>
2026-02-08 10:23:03 +0530

    update blog page

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -3,20 +3,30 @@ import HeroSection from '../components/blogpage/HeroSection';
 import SearchBlog from '../components/blogpage/SearchBlog';
 import LatestBlog from '../components/blogpage/LatestBlog';
 import MoreBlog from '../components/blogpage/MoreBlog';
+import { Helmet } from "react-helmet";
+
 
 const BlogPage = () => {
    
   return (
-    <>
+  
     
         <>
+ {/* Meta data of the blog page */}
+      <Helmet>
+        <title>Saket Mishra: Blogs</title>
+        <meta
+          name="description"
+          content="All the technical blogs written by Saket Mishra. On the topics like Web Dev, Mobile Dev, Backend, Docker, Kubernetes, GitHub, Salesforce,Cloud as well as Daily Tech news etc"
+        />
+      </Helmet>
+
         <HeroSection/>
         {/* <SearchBlog/> */}
         <LatestBlog/>
         {/* <MoreBlog/> */}
 
         </>
-    </>
   );
 };
 
